b4ad99fa9f1cd1b466e50821b067320c94a2e1c0
olzhastype <37653748+olzhastype@users.noreply.github.com>
2023-08-20 22:33:23 +0600

    2 files modified

--- src/app/components/chart-view/chart-view.component.ts
+++ src/app/components/chart-view/chart-view.component.ts
@@ -29,51 +29,140 @@ export class ChartViewComponent implements OnInit {
       fontSize: 20,
     });
     textNode.on('dblclick dbltap', () => {
-      // create textarea over canvas with absolute position
+      textNode.hide();
 
+      // create textarea over canvas with absolute position
       // first we need to find position for textarea
       // how to find it?
 
       // at first lets find position of text node relative to the stage:
-      var textPosition = textNode.getAbsolutePosition();
-
-      // then lets find position of stage container on the page:
-      var stageBox = stage.container().getBoundingClientRect();
+      var textPosition = textNode.absolutePosition();
 
       // so position of textarea will be the sum of positions above:
       var areaPosition = {
-        x: stageBox.left + textPosition.x,
-        y: stageBox.top + textPosition.y,
+        x: stage.container().offsetLeft + textPosition.x,
+        y: stage.container().offsetTop + textPosition.y,
       };
 
       // create textarea and style it
       var textarea = document.createElement('textarea');
       document.body.appendChild(textarea);
 
+      // apply many styles to match text on canvas as close as possible
+      // remember that text rendering on canvas and on the textarea can be different
+      // and sometimes it is hard to make it 100% the same. But we will try...
       textarea.value = textNode.text();
       textarea.style.position = 'absolute';
       textarea.style.top = areaPosition.y + 'px';
       textarea.style.left = areaPosition.x + 'px';
-      textarea.style.width = textNode.width().toString();
+      textarea.style.width = textNode.width() - textNode.padding() * 2 + 'px';
+      textarea.style.height =
+        textNode.height() - textNode.padding() * 2 + 5 + 'px';
+      textarea.style.fontSize = textNode.fontSize() + 'px';
+      textarea.style.border = 'none';
+      textarea.style.padding = '0px';
+      textarea.style.margin = '0px';
+      textarea.style.overflow = 'hidden';
+      textarea.style.background = 'none';
+      textarea.style.outline = 'none';
+      textarea.style.resize = 'none';
+      textarea.style.lineHeight = textNode.lineHeight().toString();
+      textarea.style.fontFamily = textNode.fontFamily();
+      textarea.style.transformOrigin = 'left top';
+      textarea.style.textAlign = textNode.align();
+      textarea.style.color = textNode.fill();
+      let rotation = textNode.rotation();
+      var transform = '';
+      if (rotation) {
+        transform += 'rotateZ(' + rotation + 'deg)';
+      }
+
+      var px = 0;
+      // also we need to slightly move textarea on firefox
+      // because it jumps a bit
+      var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+      if (isFirefox) {
+        px += 2 + Math.round(textNode.fontSize() / 20);
+      }
+      transform += 'translateY(-' + px + 'px)';
+
+      textarea.style.transform = transform;
+
+      // reset height
+      textarea.style.height = 'auto';
+      // after browsers resized it we can set actual value
+      textarea.style.height = textarea.scrollHeight + 3 + 'px';
 
       textarea.focus();
 
+      function removeTextarea() {
+        textarea.parentNode.removeChild(textarea);
+        window.removeEventListener('click', handleOutsideClick);
+        textNode.show();
+      }
+
+      function setTextareaWidth(newWidth) {
+        if (!newWidth) {
+          // set width for placeholder
+          newWidth = textNode.width() * textNode.fontSize();
+        }
+        // some extra fixes on different browsers
+        var isSafari = /^((?!chrome|android).)*safari/i.test(
+          navigator.userAgent
+        );
+        var isFirefox =
+          navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        if (isSafari || isFirefox) {
+          newWidth = Math.ceil(newWidth);
+        }
+
+        var isEdge = false;
+        if (isEdge) {
+          newWidth += 1;
+        }
+        textarea.style.width = newWidth + 'px';
+      }
+
       textarea.addEventListener('keydown', function (e) {
         // hide on enter
-        if (e.keyCode === 13) {
+        // but don't hide on shift + enter
+        if (e.keyCode === 13 && !e.shiftKey) {
           textNode.text(textarea.value);
-          document.body.removeChild(textarea);
+          removeTextarea();
+        }
+        // on esc do not set value back to node
+        if (e.keyCode === 27) {
+          removeTextarea();
         }
       });
+
+      textarea.addEventListener('keydown', function (e) {
+        let scale = textNode.getAbsoluteScale().x;
+        setTextareaWidth(textNode.width() * scale);
+        textarea.style.height = 'auto';
+        textarea.style.height =
+          textarea.scrollHeight + textNode.fontSize() + 'px';
+      });
+
+      function handleOutsideClick(e) {
+        if (e.target !== textarea) {
+          textNode.text(textarea.value);
+          removeTextarea();
+        }
+      }
+      setTimeout(() => {
+        window.addEventListener('click', handleOutsideClick);
+      });
     });
-    var tr = new Konva.Transformer();
+    var tra = new Konva.Transformer();
+
     Konva.Image.fromURL(url, (img: Konva.Image) => {
       console.log(img);
       toolTip.draggable(true);
       toolTip.add(img);
-      toolTip.add(tr);
+      layer.add(tra);
       toolTip.add(textNode);
-      tr.nodes([toolTip]);
+      tra.nodes([toolTip]);
     });
     layer.add(toolTip);
   }
